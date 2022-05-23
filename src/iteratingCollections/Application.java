9df123843543aea12c93d11234fb7efc36dcfdf8
Gill Erick <erick@sendyit.com>
2022-05-23 12:42:12 +0300

    Iterating collections: Add iterator example

---
+++ src/iteratingCollections/Application.java
@@ -0,0 +1,27 @@
+package iteratingCollections;
+
+import collectionMethods.challenge.Room;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+
+public class Application {
+  public static void main(String[] args) {
+
+    Room cambridge = new Room("Cambridge", "Premiere Room", 4, 175.00);
+    Room manchester = new Room("Manchester", "Suite", 5, 250.00);
+    Room piccadilly = new Room("Piccadilly", "Premiere Room", 3, 125.00);
+    Room oxford = new Room("Oxford", "Suite", 5, 225.0);
+
+    Collection<Room> rooms = new ArrayList<>(Arrays.asList(cambridge, manchester, piccadilly, oxford));
+
+    //It is important to note that this factory method returns a new instance of an iterator
+    Iterator<Room> iterator = rooms.iterator();
+    System.out.println(iterator.next().getName()); //Cambridge
+    System.out.println(iterator.next().getName()); //Manchester
+    System.out.println(iterator.next().getName()); //Piccadilly
+    System.out.println(iterator.next().getName()); //Oxford
+  }
+}
